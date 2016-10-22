97e56f54a4fc375aee245946e1e54771b3a1a8e0
Phyyl <phyyl@live.ca>
2016-10-22 12:29:59 -0400

    Fixed namespaces and removed unused files

--- DnsUtils/Services/INameResolver.cs
+++ DnsUtils/Services/INameResolver.cs
@@ -3,7 +3,7 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace DnsUtils
+namespace DnsUtils.Services
 {
 	public interface INameResolver
 	{
--- DnsUtils/Services/NameResolving.cs
+++ DnsUtils/Services/NameResolving.cs
@@ -4,7 +4,7 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace DnsUtils
+namespace DnsUtils.Services
 {
 	public static class NameResolving
 	{
